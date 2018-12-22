8e671a2960c31b180031d211382081e068b5ecb6
ksh1101 <0solarboy0@naver.com>
2018-12-22 14:38:49 +0900

    StoreAdapter 생성자 만들고, 필요 변수 세팅 마무리

--- app/src/main/java/kr/tjeit/listview02_20181222_03/adapters/StoreAdapter.java
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/adapters/StoreAdapter.java
@@ -1,6 +1,7 @@
 package kr.tjeit.listview02_20181222_03.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
@@ -10,7 +11,15 @@ import kr.tjeit.listview02_20181222_03.datas.Store;
 
 public class StoreAdapter extends ArrayAdapter<Store> {
 
+    Context mContext;
+    List<Store> mList;
+    LayoutInflater inf;
+
     public StoreAdapter(Context mContext, List<Store> mList){
-        super(mContext, R.layout.);
+        super(mContext, R.layout.store_list_item, mList);
+
+        this.mContext = mContext;
+        this.mList = mList;
+        this.inf = LayoutInflater.from(mContext);
     }
 }
