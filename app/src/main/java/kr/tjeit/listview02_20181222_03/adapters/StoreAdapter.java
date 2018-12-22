1bb075f01127f01436c548f79c446668186b87d9
ksh1101 <0solarboy0@naver.com>
2018-12-22 14:20:00 +0900

    스토어 목록을 보여주는데 사용할 XML 파일 작성

---
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/adapters/StoreAdapter.java
@@ -0,0 +1,16 @@
+package kr.tjeit.listview02_20181222_03.adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import kr.tjeit.listview02_20181222_03.R;
+import kr.tjeit.listview02_20181222_03.datas.Store;
+
+public class StoreAdapter extends ArrayAdapter<Store> {
+
+    public StoreAdapter(Context mContext, List<Store> mList){
+        super(mContext, R.layout.);
+    }
+}
