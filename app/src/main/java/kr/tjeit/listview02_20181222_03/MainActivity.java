86e2a7eb207e3413a8431b614322772964482d54
ksh1101 <0solarboy0@naver.com>
2018-12-22 15:08:07 +0900

    임시 데이터 삽입 코드

--- app/src/main/java/kr/tjeit/listview02_20181222_03/MainActivity.java
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/MainActivity.java
@@ -32,6 +32,20 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        fillStores();
+    }
+
+//    가게 데이터를 채우기 위한 임시 메소드
+    void fillStores(){
+        storeList.clear();
+
+        storeList.add(new Store("BBQ","종로구 10길", 4, "오후 2:00 ~ 오전 1:00", "01012345678"));
+        storeList.add(new Store("네네치킨","종로구 1길", 4, "오후 2:00 ~ 오전 4:00", "01098765418"));
+        storeList.add(new Store("노랑통닭","종로구 12길", 4, "오후 2:00 ~ 오전 2:00", "01012345123"));
+        storeList.add(new Store("디디치킨","종로구 4길", 4, "오후 2:00 ~ 오전 1:00", "01012654678"));
+        storeList.add(new Store("BHC","종로구 8길", 4, "오후 4:00 ~ 오전 1:00", "01012345678"));
+        storeList.add(new Store("교촌치킨","종로구 22길", 4, "오후 5:00 ~ 오전 1:00", "01014567678"));
+        storeList.add(new Store("굽네치킨","종로구 7길", 4, "오후 6:00 ~ 오전 2:00", "01045675678"));
 
     }
 
--- app/src/main/java/kr/tjeit/listview02_20181222_03/adapters/StoreAdapter.java
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/adapters/StoreAdapter.java
@@ -2,7 +2,12 @@ package kr.tjeit.listview02_20181222_03.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -21,5 +26,36 @@ public class StoreAdapter extends ArrayAdapter<Store> {
         this.mContext = mContext;
         this.mList = mList;
         this.inf = LayoutInflater.from(mContext);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View row = convertView;
+
+        if(row==null){
+            row = inf.inflate(R.layout.store_list_item, null);
+        }
+
+        TextView storeNameTxt = row.findViewById(R.id.storeNameTxt);
+        TextView storeAddressTxt = row.findViewById(R.id.storeAddressTxt);
+        Button storeCallBtn = row.findViewById(R.id.storeCallBtn);
+        TextView storeOpenTimeTxt = row.findViewById(R.id.storeOpenTimeTxt);
+
+        final Store data = mList.get(position);
+
+        storeNameTxt.setText(data.getName());
+        storeAddressTxt.setText(data.getAddress());
+        storeOpenTimeTxt.setText(data.getOpenAndCloseTime());
+
+        storeCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, data.getPhoneNum()+"에게 전화를 겁니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return row;
     }
 }
